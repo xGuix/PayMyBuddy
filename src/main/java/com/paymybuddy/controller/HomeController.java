59a99b1ae66c77351cfa97bc07ec52ef0156347d
Guix <88253846+xGuix@users.noreply.github.com>
2022-03-02 00:53:09 +0100

    PRofil updatein progress

--- src/main/java/com/paymybuddy/controller/HomeController.java
+++ src/main/java/com/paymybuddy/controller/HomeController.java
@@ -1,14 +1,24 @@
 package com.paymybuddy.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController
 {
+//	private final UserService userService;
+//	
+//	public HomeController(UserService userService)
+//	{
+//		this.userService = userService;
+//	}
+//	
 	@GetMapping({"/", "homepage"})
-	public String homepage()
+	public String homepage(Model model)
 	{ 
+		model.addAttribute("success", "Success!");
+		//userService.updateUser("user", user);
 		return "homepage";
 	}
 	
