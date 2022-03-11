48b372ad1c39e810e98b3cadf67e43d058879454
Guix <88253846+xGuix@users.noreply.github.com>
2022-03-11 01:34:02 +0100

    Unit Test in progress

--- src/test/java/com/paymybuddy/controller/HomeControllerTest.java
+++ src/test/java/com/paymybuddy/controller/HomeControllerTest.java
@@ -1,47 +1,35 @@
 package com.paymybuddy.controller;
 
-import static org.mockito.Mockito.RETURNS_DEFAULTS;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-
-import com.paymybuddy.model.User;
-import com.paymybuddy.service.UserService;
 
 @WebMvcTest(controllers = HomeController.class)
 class HomeControllerTest
 {
-	@Autowired
-	MockMvc mockMvc;
-	
-	@MockBean
-	UserService userService;
-
-	static User user;
-
-	@BeforeEach
-	void setupTest() {
-		user = new User();
-	}
-	
-	@Test
-	void whenCallFirestationAlert() throws Exception {
-		
-		when(userService.getUserByEmail("gb@paymybuddy.com"))
-				.then(RETURNS_DEFAULTS);
-		
-	    mockMvc.perform(get("/homepage")
-	    		.contentType(MediaType.APPLICATION_JSON)
-				.param("email", "gb@paymybuddy.com"))
-	        	.andExpect(status().isOk())
-	    		.andReturn();
-	}
+//	@Autowired
+//	MockMvc mockMvc;
+//	
+//	@MockBean
+//	UserService userService;
+//
+//	User userTest;
+//	BigDecimal balance = BigDecimal.ZERO;
+//	List<User> userListTest;
+//
+//	@BeforeEach
+//	void setupTest()
+//	{
+//		userTest = new User("Guix","Debrens","Orion","gb@paymybuddy.com", "Admin", balance, userListTest);
+//	}
+//	
+//	@Test
+//	void whenCallHomepage() throws Exception
+//	{	
+//		when(userService.getUserByEmail("gb@paymybuddy.com"))
+//				.then(RETURNS_DEFAULTS);
+//		
+//	    mockMvc.perform(get("/homepage")
+//				.param("email", "gb@paymybuddy.com"))
+//	        	.andExpect(status().isOk())
+//	    		.andReturn();
+//	}
 }
--- src/test/java/com/paymybuddy/service/UserServiceTest.java
+++ src/test/java/com/paymybuddy/service/UserServiceTest.java
@@ -1,4 +1,114 @@
 package com.paymybuddy.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.paymybuddy.dto.SignupDTO;
+import com.paymybuddy.model.User;
+import com.paymybuddy.repository.UserRepository;
+
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTest 
-{}
+{
+    @InjectMocks
+    UserService userService;
+    
+    @Mock
+    UserRepository userRepository;
+   
+	User userTest;
+	User userFriendTest;
+	Integer userId = 1;
+	Integer userFriendId = 2;
+	BigDecimal balance = BigDecimal.ZERO;
+	List<User> userListTest;
+	SignupDTO signupDto;
+    
+	@BeforeEach
+	void setupTest()
+	{
+		userListTest = new ArrayList<>();
+		userTest = new User("Guix","Debrens","Orion","gb@paymybuddy.com", "Admin", balance, userListTest);
+		userFriendTest = new User("Bob","Lazar","Groomlake","bl@paymybuddy.com", "Zone51", balance, userListTest);
+		signupDto = new SignupDTO("Guix","Debrens","Orion","gb@paymybuddy.com", "Admin");
+	}
+ 
+	@Test
+	void TestIfGetUserByIdReturnUser()
+	{
+		when(userRepository.getById(1)).thenReturn(userTest);
+		
+		User userToTest = userService.getUserById(userId);
+		
+		assertEquals(userTest, userToTest);
+		verify(userRepository, Mockito.times(1)).getById(userId);
+	}
+	
+	@Test
+	void TestIfGetUserByEmailReturnUser()
+	{
+		when(userRepository.findByEmail(userTest.getEmail())).thenReturn(userTest);
+		
+		User userToTest = userService.getUserByEmail(userTest.getEmail());
+		
+		assertEquals(userTest, userToTest);
+		verify(userRepository, Mockito.times(1)).findByEmail(userTest.getEmail());
+	}
+	
+	@Test
+	void TestIfValidateUserReturnNotFoundString()
+	{
+		String stringTest = "Not Found";
+		String stringUser = userService.validateUser(signupDto);
+		
+		assertEquals(stringTest,stringUser);
+	}
+	
+	@Test
+	void TestIfValidateUserReturnAlreadyUsedString()
+	{
+		when(userRepository.findByEmail(userTest.getEmail())).thenReturn(userTest);
+		String stringTest = "Email address already used!";
+		String stringUser = userService.validateUser(signupDto);
+		
+		assertEquals(stringTest,stringUser);
+		verify(userRepository, Mockito.times(1)).findByEmail(userTest.getEmail());
+	}
+		
+	@Test
+	void TestIfAddUserReturnNewUser()
+	{
+		when(userService.getUserByEmail(userTest.getEmail())).thenReturn(userTest);
+		
+		User userToTest = userService.addUser(signupDto);
+		
+		assertEquals(userTest, userToTest);
+	}
+	
+	@Test
+	void TestIfupdateUserReturnUser()
+	{
+		when(userRepository.findByEmail(userTest.getEmail())).thenReturn(userTest);
+		
+		User userToTest = userService.updateUser(
+				userTest.getEmail(),
+				userTest.getFirstname(),userTest.getLastname(),
+				userTest.getCity(), userTest.getEmail());
+		
+		assertEquals(userTest, userToTest);
+		verify(userRepository, Mockito.times(1)).findByEmail(userTest.getEmail());
+	}
+}
